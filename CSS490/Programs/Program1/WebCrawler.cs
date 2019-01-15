42775578e0e764c468ed871ba5d76cff964e37f3
Caleb <moorec96@uw.edu>
2019-01-15 14:29:03 -0800

    Almost done

--- CSS490/Programs/Program1/WebCrawler.cs
+++ CSS490/Programs/Program1/WebCrawler.cs
@@ -14,7 +14,9 @@ namespace WebCrawler
     {
 
         //private Dictionary<String, bool> visitedSites;
-
+//===================================================================================================
+	//
+//===================================================================================================
         static void Main(string[] args)
         {
             if(args.Length < 2)
@@ -28,27 +30,23 @@ namespace WebCrawler
             
             string hostname = args[0];
             int numHops = Int32.Parse(args[1]);
-			string htmlPage = getHTML(hostname, numHops);
+			hostname = prepareHost(hostname);
+			IPHostEntry host = getIP(getHostName(hostname));
+			string htmlPage = getHTML(hostname,host, numHops);
             
+			Console.WriteLine(htmlPage);
             //Hit any key to exit
 			Console.WriteLine("Press any key to exit");
             Console.ReadKey();
 			
         }
 
-		static Tuple<Socket,IPHostEntry> getSock(string hostname){
+//===================================================================================================
+	//
+//===================================================================================================
+		static Socket getSock(IPHostEntry host){
             Socket sock = null;
-			IPHostEntry host = null;
 			int port = 80;
-			try{
-				host = Dns.GetHostEntry(hostname);
-			}
-			catch(SocketException e){
-				Console.WriteLine("Invalid Host");
-				Console.WriteLine("Press any key to exit");
-                Console.ReadKey();
-                Environment.Exit(0);
-			}
             if(host == null)
             {
                 Console.Write("Invalid Host");
@@ -75,25 +73,28 @@ namespace WebCrawler
                 Console.ReadKey();
                 Environment.Exit(0);
             }
-			Tuple<Socket,IPHostEntry> retVal = new Tuple<Socket,IPHostEntry>(sock,host);
-			return retVal;
+			return sock;
 		}
-
+//===================================================================================================
+	//
+//===================================================================================================
 		static string prepareHost(string host){
-			if(host.Contains("http://") || host.Contains("HTTP://")){
-				host = host.Substring(7);
-				Console.WriteLine(host);
-			}
-			else if(host.Contains("https://") || host.Contains("HTTPs://")){
-				host = host.Substring(8);
+			//Console.WriteLine("Test: " + host);
+			if(host.Contains("://")){
+				host = host.Substring(host.IndexOf("://")+ 3);
+				//Console.WriteLine(host);
 			}
 
-			if(!host.Contains("www.") || !host.Contains("WWW.")){
+			if(!host.Contains("www.") && !host.Contains("WWW.")){
 				host = "www." + host;
 			}
+			//Console.WriteLine("Test After: " + host);
 			return host;
 		}
 
+//===================================================================================================
+	//
+//===================================================================================================
 		static string getPath(string host){
 			string path = "";
 			if(host.Contains('/')){
@@ -105,37 +106,54 @@ namespace WebCrawler
 			return path;
 		}
 
-		static string getHTML(string hostname, int numHops){
+//===================================================================================================
+	//
+//===================================================================================================
+		static string getHTML(string hostname, IPHostEntry host, int numHops){
+			
+			string resp = makeHTTPRequest(hostname,host);
 			if(numHops == 0){
-				return parseHTML(hostname);
+				return parseHTML(resp);
 			}
-
-			string resp = makeHTTPRequest(hostname);
-
 			MatchCollection matches = getLinks(resp);
 			if(matches.Count == 0){
 				return parseHTML(resp);
 			}
 			foreach(Match match in matches){
 				GroupCollection groups = match.Groups;
-				
-				Console.WriteLine(groups[0]);
+				//Console.WriteLine(groups[1].ToString());
+				string hostN = prepareHost(groups[1].ToString());
+				//Console.WriteLine(hostN);
+				host = getIP(getHostName(hostN));
+				if(host != null){
+					return getHTML(hostN, host, numHops-1);
+				}
 			}
-			return "";
-		}
-		
-		static string parseHTML(string hostname){
-			return parseHTML(makeHTTPRequest(hostname));
+			return parseHTML(resp);
 		}
 
-		static string makeHTTPRequest(string hostname){
-			hostname = prepareHost(hostname);
+//===================================================================================================
+	//
+//===================================================================================================		
+		static string parseHTML(string resp){
+			if(resp.Contains("\r\n\r\n")){
+				return resp.Substring(resp.IndexOf("\r\n\r\n"));
+			}
+			else{
+				return "The HTML could not be found.";
+			}
+		}
+
+//===================================================================================================
+	//
+//===================================================================================================
+		static string makeHTTPRequest(string hostname, IPHostEntry hostEntry){
 			string path = getPath(hostname);
-			Tuple<Socket,IPHostEntry> hostInfo = getSock(hostname);
-			Socket sock = hostInfo.Item1;
-			string host = hostInfo.Item2.HostName;
+			Socket sock = getSock(hostEntry);
 
-			string request = "GET " + path + " HTTP/1.1\r\nHost: " + host + 
+			//Console.WriteLine(hostEntry.HostName + path);
+
+			string request = "GET " + path + " HTTP/1.1\r\nHost: " + hostEntry.HostName + 
 				"\r\nConnection: keep-alive\r\nAccept: text/html\r\n\r\n";
             byte[] dataToSend = Encoding.ASCII.GetBytes(request);
             sock.Send(dataToSend);
@@ -155,10 +173,41 @@ namespace WebCrawler
 			return resp;
 		}
 
+//===================================================================================================
+	//
+//===================================================================================================
 		static MatchCollection getLinks(string resp){
 			//string regExp = "href=[\'"]?([^\'" >]+)";
 			Regex rx = new Regex("href=\"([^\"]*)",RegexOptions.IgnoreCase);
 			return rx.Matches(resp);
 		}	
+
+//===================================================================================================
+	//
+//===================================================================================================
+		static IPHostEntry getIP(string hostname){
+			IPHostEntry host = null;
+			try{
+				host = Dns.GetHostEntry(hostname);
+			}
+			catch(SocketException e){
+				Console.WriteLine("Invalid Hostname");
+				Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+                Environment.Exit(0);
+			}
+
+			return host;
+		}
+
+//===================================================================================================
+	//
+//===================================================================================================
+		static string getHostName(string host){
+			if(host.Contains('/')){
+				host = host.Substring(0,host.IndexOf('/'));
+			}
+			return host;
+		}
     }
 }
