a2975ab568428cd98d8372666f09e46d986d6e38
Caleb <moorec96@uw.edu>
2019-01-16 20:14:03 -0800

    New Program

---
+++ WebCrawler.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+
+namespace WebCrawler
+{
+    class WebCrawler
+    {
+
+        static private Dictionary<string, bool> visitedSites;
+//===================================================================================================
+	//
+//===================================================================================================
+        static void Main(string[] args)
+        {
+            
+        }
+	}
+}
